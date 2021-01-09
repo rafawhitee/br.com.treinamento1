bbed94f35aa0ebbb49cfc62d0f9e58f5ec5e42f5
Rafael Nunes <46350003+rafawhitee@users.noreply.github.com>
2021-01-09 03:19:06 -0300

    serviço de produtos teste

---
+++ src/br/com/treinamento1/service/helloworld/HelloWorld.java
@@ -0,0 +1,27 @@
+package br.com.treinamento1.service.helloworld;
+
+import lumis.portal.PortalException;
+import lumis.portal.serviceinterface.GenericServiceInterface;
+import lumis.portal.serviceinterface.IServiceInterfaceRenderRequest;
+import lumis.portal.serviceinterface.IServiceInterfaceRenderResponse;
+import lumis.portal.serviceinterface.ServiceInterfaceException;
+
+public class HelloWorld extends GenericServiceInterface {
+
+	@Override
+	public void render(IServiceInterfaceRenderRequest request, IServiceInterfaceRenderResponse response)
+			throws ServiceInterfaceException, PortalException {
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<div>");
+			for (int i = 0; i < 10; i++)
+				sb.append("<span> Parágrafo " + i + " </span>");
+
+			sb.append("</div>");
+			response.getWriter().write(sb.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
